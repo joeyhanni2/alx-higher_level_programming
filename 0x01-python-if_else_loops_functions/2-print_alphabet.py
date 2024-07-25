1b5f372ba55adadb24168e6b9d0fa126edc2bf32
joeyhanni2 <joeyhanni@gmail.com>
2024-07-25 16:37:22 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,7 @@
-#!/user/bin/python
+#!/usr/bin/python3
 
 # a program that prints the ASCII alphabet 
 #in lowercase letters
 
-for i in range (97, 123):
-
-	print(chr(i).format(), end=' ')
+for i in range(97, 123):
+	print(chr(i).format(), end="")
