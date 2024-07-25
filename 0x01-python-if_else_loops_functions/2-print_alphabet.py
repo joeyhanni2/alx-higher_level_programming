218cef135bfcc77660de3e34ba707de8e8e5ad09
joeyhanni2 <joeyhanni@gmail.com>
2024-07-25 06:12:38 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,8 @@
 #!/user/bin/python
 
-# a program tht prints the ASCII alphabet 
+# a program that prints the ASCII alphabet 
 #in lowercase letters
 
 for i in range (97, 123)
 
+print("{:c}".format(ascii_code), end='')
