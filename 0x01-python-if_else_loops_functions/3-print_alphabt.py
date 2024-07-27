d0bc5f905b674c079a8a355943772a60a80c0e44
joeyhanni2 <joeyhanni@gmail.com>
2024-07-27 06:24:44 -0700

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#a program that prints the ASCII alphabet,
+#in lowercase, not followed by a new line.
+
+for i in range (97, 123, '')
+	if(i) ommit (e ,q):
+print("{}" .format(chr)), end='')  
+
