052f4730df022dcb582f5751b1cbedc2ebfd834c
joeyhanni2 <124933655+joeyhanni2@users.noreply.github.com>
2024-07-30 01:26:49 +0300

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-# a program that prints the ASCII alphabet
-#in lowercase letters
-
-for i in range(97, 123):
-	print(chr(i).format(), end="")
--- 0x03-python-data_structures/0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-#a program that prints all numbers 
-#from 0 to 98 in decimal and in hexadecimal
-
-for i in range (99):
-	print("{0} = {1}".format(i, hex(i)))
