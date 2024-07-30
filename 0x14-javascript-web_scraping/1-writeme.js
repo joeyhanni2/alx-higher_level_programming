99301ca963fcc16ad13937c0274d6fea3788fce3
joeyhanni2 <joeyhanni@gmail.com>
2024-07-30 15:26:23 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-	if (error) console.log(error);
+const filename = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
 });
