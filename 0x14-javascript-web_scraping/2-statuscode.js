08286a89b378981fdbdb34712c0cbce1fa570e80
joeyhanni2 <joeyhanni@gmail.com>
2024-07-30 15:28:45 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(`code: ${response.statusCode}`);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
