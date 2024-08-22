97b34cf997dbca954d7216bcd7a054e1d463ae4e
joeyhanni2 <124933655+joeyhanni2@users.noreply.github.com>
2024-08-22 18:01:17 +0300

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,10 @@ import sys
 
 
 if __name__ == "__main__":
-   values = {'email': sys.argv[2]}
-   data   = parse.urlencode(values)
-   data   = data.encode('ascii')
-   req    = request.Request(sys.argv[1], data)
-   with request.urlopen(req) as response:
-     body = response.read()
-     print(body.decode('utf-8'))
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
