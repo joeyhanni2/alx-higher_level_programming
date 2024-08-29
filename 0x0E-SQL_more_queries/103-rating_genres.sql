4f2bc1a2ef8f52f15885f9f9f757fb48dbb5aeeb
joeyhanni2 <joeyhanni@gmail.com>
2024-08-29 16:09:01 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
--- Script that lists genre by their rating
+-- Script that lists genres by their rating
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
 FROM tv_genres
 JOIN tv_show_genres
@@ -6,6 +6,6 @@ JOIN tv_show_genres
 JOIN tv_shows
      ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_show_ratings
-     ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_genre.name
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
