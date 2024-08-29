61200f360fbdd73e38c647b73540af37f8e8caad
joeyhanni2 <joeyhanni@gmail.com>
2024-08-29 15:55:16 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script that lists genres and number of shows
-SELECT tv_genre.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows -- Query to join  tables
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows -- Query to join tables
 FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
