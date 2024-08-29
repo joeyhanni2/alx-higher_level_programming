c6ea6e6af7a755dd39bd39e419f3c7ab1f14e636
joeyhanni2 <joeyhanni@gmail.com>
2024-08-29 15:50:46 -0700

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
--- Script that lists all tv shows contained in a database.
-SELECT tv_shows.title, tv_show_genre.genre_id -- Query to join shows and genre
+-- Script that lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show-genres.show_id
-WHERE tv_show_genre.genre_id is NULL
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
 ORDER BY tv_shows.title;
